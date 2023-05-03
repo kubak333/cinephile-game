382d2843a275835cb3aa851d0cf0d4ec2060dfbc
Jakub Kucharczyk <kubak333@gmail.com>
2023-05-03 12:32:01 +0200

    Dodanie barów zdrowia i pęchęrza oraz funkcjonalności z nimi związanymi

--- game/script.js
+++ game/script.js
@@ -29,7 +29,6 @@ function scoreFunction() {
 const scoreInterval = setInterval(scoreFunction, 100);
 
 
-
 // MECHANICS
 
 const map = document.querySelector("#map");
@@ -77,4 +76,70 @@ window.addEventListener("resize", () => {
 });
 
 
+// HELTHBAR
 
+const healthbar = document.querySelector('.health');
+let healthPoints = 80;
+
+
+function healthFunction() {
+  healthPoints--;
+  healthbar.style.height = `${healthPoints}%`;
+  if (healthPoints === 0) {
+    clearInterval(countdownTimer);
+    clearInterval(scoreInterval);
+    alert(`Zemdlałeś z głodu! Twój wynik to ${scoreCounter}! Odśwież stronę, by zagrać ponownie.`);
+        }
+}
+
+const healthInterval = setInterval(healthFunction, 700);
+
+
+
+
+// BLADDERBAR
+
+const bladderbar = document.querySelector('.bladder');
+let bladderPoints = 0;
+
+
+function bladderFunction() {
+  bladderPoints++;
+  bladderbar.style.height = `${bladderPoints}%`;
+  if (bladderPoints === 80) {
+    clearInterval(countdownTimer);
+    clearInterval(scoreInterval);
+    clearInterval(bladderInterval);
+    alert(`Nie zdążyleś do toalety! Twój wynik to ${scoreCounter}! Odśwież stronę, by zagrać ponownie.`);
+        }
+}
+
+const bladderInterval = setInterval(bladderFunction,1000);
+
+
+
+// PAUZA
+
+let isPaused = false;
+
+function pauseGame() {
+  if (!isPaused) {
+    clearInterval(countdownTimer);
+    clearInterval(scoreInterval);
+    clearInterval(healthInterval);
+    clearInterval(bladderInterval);
+    isPaused = true;
+  } else {
+    countdownTimer = setInterval(counter, 1000);
+    scoreInterval = setInterval(scoreFunction, 100);
+    healthInterval = setInterval(healthFunction, 700);
+    bladderInterval = setInterval(bladderFunction, 1000);
+    isPaused = false;
+  }
+}
+
+document.addEventListener("keydown", function(event) {
+  if (event.code === "KeyP") {
+    pauseGame();
+  }
+});
