b88a9e04adc9d9a85d496bfbd27c34f1f33defa7
Jakub Kucharczyk <kubak333@gmail.com>
2023-08-05 15:27:20 +0200

    Moment, w którtym wskazywana jest nowa sala i zostało jedynie wdrożenie NPC

--- game/script.js
+++ game/script.js
@@ -1,5 +1,3 @@
-// TIMER
-
 const timerElement = document.getElementById('timer');
 let timeLeft = 30;
 
@@ -10,33 +8,27 @@ function counter() {
 
 const countdownTimer = setInterval(counter, 1000);
 
-
-// SCORE
-
 const scoreElement = document.getElementById('score');
 let scoreCounter = 0;
 
 function scoreFunction() {
   scoreCounter++;
   scoreElement.textContent = scoreCounter;
-  if (timeLeft === 0) {
+  if (timeLeft <= 0) {
     clearInterval(countdownTimer);
     clearInterval(scoreInterval);
+    clearInterval(healthInterval);
+    clearInterval(bladderInterval);
     alert(`Czas minął! Twój wynik to ${scoreCounter}! Odśwież stronę, by zagrać ponownie.`);
-        }
+  }
 }
 
 const scoreInterval = setInterval(scoreFunction, 100);
 
-
-// MECHANICS
-
 const map = document.querySelector("#map");
 const character = document.querySelector("#character");
-
 const characterWidth = character.offsetWidth;
 const characterHeight = character.offsetHeight;
-
 let x = (map.offsetWidth - characterWidth) / 2;
 let y = (map.offsetHeight - characterHeight) / 2;
 
@@ -75,12 +67,8 @@ window.addEventListener("resize", () => {
   y = (map.offsetHeight - characterHeight) / 2;
 });
 
-
-// HELTHBAR
-
 const healthbar = document.querySelector('.health');
-let healthPoints = 80;  
-
+let healthPoints = 80;
 
 function healthFunction() {
   healthPoints--;
@@ -88,22 +76,16 @@ function healthFunction() {
   if (healthPoints === 0) {
     clearInterval(countdownTimer);
     clearInterval(scoreInterval);
-    clearInterval(healthInterval)
+    clearInterval(healthInterval);
     clearInterval(bladderInterval);
     alert(`Zemdlałeś z głodu! Twój wynik to ${scoreCounter}! Odśwież stronę, by zagrać ponownie.`);
-        }
+  }
 }
 
 const healthInterval = setInterval(healthFunction, 700);
 
-
-
-
-// BLADDERBAR
-
 const bladderbar = document.querySelector('.bladder');
 let bladderPoints = 0;
-
 
 function bladderFunction() {
   bladderPoints++;
@@ -111,57 +93,47 @@ function bladderFunction() {
   if (bladderPoints === 80) {
     clearInterval(countdownTimer);
     clearInterval(scoreInterval);
-    clearInterval(healthInterval)
+    clearInterval(healthInterval);
     clearInterval(bladderInterval);
     alert(`Nie zdążyleś do toalety! Twój wynik to ${scoreCounter}! Odśwież stronę, by zagrać ponownie.`);
-        }
+  }
 }
 
-const bladderInterval = setInterval(bladderFunction,1000);
-
-
-
-// NPC 
-
-
-
-// WSKAZYWANIE SALI I DODAWANIE CZASU
+const bladderInterval = setInterval(bladderFunction, 1000);
 
 const movies = ['Climax', 'Szatańskie tango', 'Midsommar', 'Wielkie piękno', 'Paterson', 'Służąca', 'Toni Ermann'];
 const movieNameElement = document.getElementById('movie-name');
 const nrSaliElement = document.getElementById('nr-sali');
 
-
-const randomMovie = movies[Math.floor(Math.random() * movies.length)];
-const randomNrSali = Math.floor(Math.random() * 6) + 1;
-
-movieNameElement.textContent = randomMovie;
-nrSaliElement.textContent = randomNrSali;
-
 let wyznaczonaSala;
+let randomNrSali = Math.floor(Math.random() * 6) + 1;
 
-if (randomNrSali === 1) { 
-  wyznaczonaSala = document.getElementsByClassName('sala1')[0]
-  }
-if (randomNrSali === 2) { 
-  wyznaczonaSala = document.getElementsByClassName('sala2')[0]
-  }
-if (randomNrSali === 3) { 
-  wyznaczonaSala = document.getElementsByClassName('sala3')[0]
-  }
-if (randomNrSali === 4) { 
-  wyznaczonaSala = document.getElementsByClassName('sala4')[0]
-  }
-if (randomNrSali === 5) { 
-  wyznaczonaSala = document.getElementsByClassName('sala5')[0]
-  }
-if (randomNrSali === 6) {
-  wyznaczonaSala = document.getElementsByClassName('sala6')[0]
-  }
+function showMovieAndNrSali() {
+  let randomMovie = movies[Math.floor(Math.random() * movies.length)];
+  movieNameElement.textContent = randomMovie;
+  nrSaliElement.textContent = randomNrSali;
 
-console.log(wyznaczonaSala)
+  if (randomNrSali === 1) { 
+    wyznaczonaSala = document.getElementsByClassName('sala1')[0];
+  }
+  if (randomNrSali === 2) { 
+    wyznaczonaSala = document.getElementsByClassName('sala2')[0];
+  }
+  if (randomNrSali === 3) { 
+    wyznaczonaSala = document.getElementsByClassName('sala3')[0];
+  }
+  if (randomNrSali === 4) { 
+    wyznaczonaSala = document.getElementsByClassName('sala4')[0];
+  }
+  if (randomNrSali === 5) { 
+    wyznaczonaSala = document.getElementsByClassName('sala5')[0];
+  }
+  if (randomNrSali === 6) {
+    wyznaczonaSala = document.getElementsByClassName('sala6')[0];
+  }
+}
 
-
+showMovieAndNrSali();
 
 function checkSalaCollision() {
   const characterRect = character.getBoundingClientRect();
@@ -175,19 +147,12 @@ function checkSalaCollision() {
   ) {
     console.log('Collision!');
     timeLeft += 15;
-  
+    randomNrSali = Math.floor(Math.random() * 6) + 1;
+    showMovieAndNrSali();
   }
 }
 
 document.addEventListener("keydown", checkSalaCollision);
-
-
-
-
-
-
-// COLLISIONS - BLADDERBAR
-
 
 const wc = document.querySelector(".wc");
 
@@ -203,17 +168,10 @@ function checkWcCollision() {
   ) {
     console.log('Collision!');
     bladderPoints = -1;
-  
   }
 }
 
 document.addEventListener("keydown", checkWcCollision);
-
-
-
-// COLLISIONS - HEALTHBAR
-
-
 
 const bistro = document.querySelector(".bistro");
 
@@ -229,8 +187,12 @@ function checkBistroCollision() {
   ) {
     console.log('Collision!');
     healthPoints = 80;
-  
   }
 }
 
 document.addEventListener("keydown", checkBistroCollision);
+
+
+
+// NPC
+
